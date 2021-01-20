9133af97add70e50fb6031e75e776533c32202d2
seamuddin <usiam30@gmail.com>
2021-01-20 13:59:37 +0600

    Django First Project. First Commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from blog.models import Post
+from blog.models import New_post
+
+admin.site.register(Post)
+admin.site.register(New_post)
+# Register your models here.
